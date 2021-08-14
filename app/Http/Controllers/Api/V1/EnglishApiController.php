d6a8e0a9aca5fbd81f347f5f95e84fd4c9998dea
nguyendx <nguyendx90@gmail.com>
2021-08-14 20:11:16 +0700

    api example

--- app/Http/Controllers/Api/V1/EnglishApiController.php
+++ app/Http/Controllers/Api/V1/EnglishApiController.php
@@ -8,13 +8,18 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Lesson;
+use App\Models\Levels;
 use App\Models\Topics;
+use App\Models\Video;
 use App\Models\Vocabulary;
 use App\Models\VocabularyCat;
 use Illuminate\Http\Request;
 
 class EnglishApiController extends Controller
 {
+    private $secret_key = 'HOvMgMDQ256jybfFokYt1kAokxtxXEA0mgy';
 
     public function __construct()
     {
@@ -26,9 +31,9 @@ class EnglishApiController extends Controller
         if (!$data) {
             return response()->json([
                 'status' => false,
-                'code' => 400,
+                'code' => 200,
                 'message' => 'No Data'
-            ], 400);
+            ], 200);
         }
         return response()->json([
             'status' => true,
@@ -44,9 +49,9 @@ class EnglishApiController extends Controller
         if (!$data) {
             return response()->json([
                 'status' => false,
-                'code' => 400,
+                'code' => 200,
                 'message' => 'No Data'
-            ], 400);
+            ], 200);
         }
         return response()->json([
             'status' => true,
@@ -62,9 +67,9 @@ class EnglishApiController extends Controller
         if (!$data) {
             return response()->json([
                 'status' => false,
-                'code' => 400,
+                'code' => 200,
                 'message' => 'No Data'
-            ], 400);
+            ], 200);
         }
         return response()->json([
             'status' => true,
@@ -72,6 +77,151 @@ class EnglishApiController extends Controller
             'message' => '',
             'data' => $data->toArray()
         ], 200);
+    }
+
+    public function courseList(Request $request)
+    {
+        $token = $request->bearerToken();
+
+        if (!empty($token)) {
+            if ($token == $this->secret_key) {
+                $data = Course::where('status', 1)->get();
+                if (!$data) {
+                    return response()->json([
+                        'status' => false,
+                        'code' => 200,
+                        'message' => 'No Data'
+                    ], 200);
+                }
+                return response()->json([
+                    'status' => true,
+                    'code' => 200,
+                    'message' => '',
+                    'data' => $data->toArray()
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'code' => 200,
+                    'message' => 'Authorization is empty.'
+                ], 200);
+            }
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 200,
+                'message' => 'Authorization not invalid.'
+            ], 200);
+        }
+    }
+
+    public function lessonList(Request $request)
+    {
+        $token = $request->bearerToken();
+
+        if (!empty($token)) {
+            if ($token == $this->secret_key) {
+                $data = Lesson::where('status', 1)->get();
+                if (!$data) {
+                    return response()->json([
+                        'status' => false,
+                        'code' => 200,
+                        'message' => 'No Data'
+                    ], 200);
+                }
+                return response()->json([
+                    'status' => true,
+                    'code' => 200,
+                    'message' => '',
+                    'data' => $data->toArray()
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'code' => 200,
+                    'message' => 'Authorization is empty.'
+                ], 200);
+            }
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 200,
+                'message' => 'Authorization not invalid.'
+            ], 200);
+        }
+    }
+
+    public function levelList(Request $request)
+    {
+        $token = $request->bearerToken();
+
+        if (!empty($token)) {
+            if ($token == $this->secret_key) {
+                $data = Levels::where('status', 1)->get();
+                if (!$data) {
+                    return response()->json([
+                        'status' => false,
+                        'code' => 200,
+                        'message' => 'No Data'
+                    ], 200);
+                }
+                return response()->json([
+                    'status' => true,
+                    'code' => 200,
+                    'message' => '',
+                    'data' => $data->toArray()
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'code' => 200,
+                    'message' => 'Authorization is empty.'
+                ], 200);
+            }
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 200,
+                'message' => 'Authorization not invalid.'
+            ], 200);
+        }
+    }
+
+    public function videoList(Request $request)
+    {
+        //$header = $request->header('token');
+        $token = $request->bearerToken();
+
+        if (!empty($token)) {
+            if ($token == $this->secret_key) {
+                $data = Video::where('status', 1)->get();
+                if (!$data) {
+                    return response()->json([
+                        'status' => false,
+                        'code' => 200,
+                        'message' => 'No Data'
+                    ], 200);
+                }
+                return response()->json([
+                    'status' => true,
+                    'code' => 200,
+                    'message' => '',
+                    'data' => $data->toArray()
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'code' => 200,
+                    'message' => 'Authorization is empty.'
+                ], 200);
+            }
+        } else {
+            return response()->json([
+                'status' => false,
+                'code' => 200,
+                'message' => 'Authorization not invalid.'
+            ], 200);
+        }
     }
 
 }
--- routes/api-routes.php
+++ routes/api-routes.php
@@ -23,6 +23,11 @@ Route::prefix('/v1')->name('apiV1.')->group(function () {
     Route::get('/vocabulary/list', [\App\Http\Controllers\Api\V1\EnglishApiController::class, 'vocabularyList'])->name('Api.get.vocabulary');
     Route::get('/vocabulary-cat/list', [\App\Http\Controllers\Api\V1\EnglishApiController::class, 'vocabularyCatList'])->name('Api.get.vocabulary.cat');
     Route::get('/topics/list', [\App\Http\Controllers\Api\V1\EnglishApiController::class, 'topicsList'])->name('Api.get.vocabulary.cat');
+
+    Route::get('/lesson/list', [\App\Http\Controllers\Api\V1\EnglishApiController::class, 'lessonList'])->name('Api.get.lesson.list');
+    Route::get('/course/list', [\App\Http\Controllers\Api\V1\EnglishApiController::class, 'courseList'])->name('Api.get.course.list');
+    Route::get('/level/list', [\App\Http\Controllers\Api\V1\EnglishApiController::class, 'levelList'])->name('Api.get.level.list');
+    Route::get('/video/list', [\App\Http\Controllers\Api\V1\EnglishApiController::class, 'videoList'])->name('Api.get.video.list');
 });
 /* END API Version 1 */
 
