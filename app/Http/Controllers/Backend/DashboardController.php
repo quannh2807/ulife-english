7039b454c87bfdc85fcd4bacaba3fb37227c33b1
quannh2807 <helgrindxxx@gmail.com>
2021-09-17 09:00:13 +0700

    Update dashboard

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Course;
+use App\Models\Lesson;
+use App\Models\Video;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index () {
-        return view('admin.dashboard.index');
+    public function index()
+    {
+        $countCourse = Course::where('status', 1)->count();
+        $countLesson = Lesson::where('status', 1)->count();
+        $countVideo = Video::where('status', 1)->count();
+
+        return view('admin.dashboard.index', [
+            'countCourse' => $countCourse,
+            'countLesson' => $countLesson,
+            'countVideo' => $countVideo,
+        ]);
     }
 }
--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,6 +1,52 @@
 @extends('admin.layouts.master')
 @section('page-title', 'Dashboard')
+@section('breadcrumb', 'Dashboard')
 
 @section('main')
+<div class="row">
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-info">
+            <div class="inner">
+                <h3>{{ $countLesson }}</h3>
 
+                <p>Bài học</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-chalkboard-teacher"></i>
+            </div>
+            <a href="{{ route('admin.lesson.index') }}" class="small-box-footer">Danh sách <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-success">
+            <div class="inner">
+                <h3>{{ $countCourse }}</h3>
+
+                <p>Khóa học</p>
+            </div>
+            <div class="icon">
+                <i class="fas fa-graduation-cap"></i>
+            </div>
+            <a href="{{ route('admin.course.index') }}" class="small-box-footer">Danh sách <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+    <!-- ./col -->
+    <div class="col-lg-3 col-6">
+        <!-- small box -->
+        <div class="small-box bg-warning">
+            <div class="inner">
+                <h3>{{ $countVideo }}</h3>
+
+                <p>Videos</p>
+            </div>
+            <div class="icon">
+                <i class="far fa-closed-captioning"></i>
+            </div>
+            <a href="{{ route('admin.video.index') }}" class="small-box-footer">Danh sách <i class="fas fa-arrow-circle-right"></i></a>
+        </div>
+    </div>
+</div>
 @endsection
